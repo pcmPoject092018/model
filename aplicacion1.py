b4d35aa24520d4580427faf9cf6a7ac846598850
pcmProject092018 <acampos@emsmxgroup.com>
2018-10-22 17:14:26 -0500

    update

--- aplicacion1.py
+++ aplicacion1.py
@@ -71,36 +71,36 @@ def pronosticoAMXL():
     error_precios=RSME(precios[0:7],precios_prediccion[0:7])	
     return render_template('pronosticoAMXL.html', values_prediccion=prediccion,values_real=real,values_precios=precios, values_precios_prediccion=precios_prediccion,labels=fecha,fechaInicio=fechaInicio,fechaFin=fechaFin,tables=[x.to_html(classes='table')],error_precios=error_precios)
 
-#@app.route("/pronosticoNAFTRAC",methods=['POST','GET']) 
-#def pronosticoAMXL():  
-#    number = request.form.get('ejemplo',type=int)
-#    dfs=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/predicciones/naftrac/*.csv'))]	
-#    prediccion=dfs[number]['prediccion']    
-#    fecha=dfs[number]['fecha'] 	      	       
-#    fechaInicio=fecha[0]  
-#    fechaFin=fecha[6]
-#    dataframes=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/dataframes/naftrac/*.csv'))]
-#    dataframesprevious=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/dataframesprevious/naftrac/*.csv'))]	
-#    prices=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/precios/naftrac/*.csv'))]  
-#    fulldataframes=[pd.concat([dataframesprevious[i],dataframes[i]]).reset_index(drop=True) for i in range(len(dataframes))]  	
-#    price=prices[number]	
-#    fulldataframe=fulldataframes[number]	
-#    real=fulldataframe['tasa']         
-#    precios=fulldataframe['precios']
-#    #build predicted prices
-#    previous_real_prices_first_half=price['precios'].tolist()[0:7]
-#    predicted_rates_first_half=prediccion.tolist()[0:7]	
-#    previous_real_prices_second_half=price['precios'].tolist()[7:]
-#    predicted_rates_second_half=prediccion.tolist()[7:]		
-#    precios_prediccion=predicted_prices(predicted_rates_first_half,previous_real_prices_first_half)+predicted_prices(predicted_rates_second_half,previous_real_prices_second_half)	
-#    #
-#    x=dataframes[number]
-#    x['pronóstico tasa']=prediccion.tolist()[7:]	
-#    x['pronóstico precios']=precios_prediccion[7:]
-#    x=x[['fecha','precios','pronóstico precios','tasa','pronóstico tasa']]		
-#    x=x.reset_index(drop=True)		
-#    error_precios=RSME(precios[0:7],precios_prediccion[0:7])	
-#    return render_template('pronosticoNAFTRAC.html', values_prediccion=prediccion,values_real=real,values_precios=precios, values_precios_prediccion=precios_prediccion,labels=fecha,fechaInicio=fechaInicio,fechaFin=fechaFin,tables=[x.to_html(classes='table')],error_precios=error_precios)
+@app.route("/pronosticoNAFTRAC",methods=['POST','GET']) 
+def pronosticoNAFTRAC():  
+    number = request.form.get('ejemplo',type=int)
+    dfs=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/predicciones/naftrac/*.csv'))]	
+    prediccion=dfs[number]['prediccion']    
+    fecha=dfs[number]['fecha'] 	      	       
+    fechaInicio=fecha[0]  
+    fechaFin=fecha[6]
+    dataframes=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/dataframes/naftrac/*.csv'))]
+    dataframesprevious=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/dataframesprevious/naftrac/*.csv'))]	
+    prices=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/precios/naftrac/*.csv'))]  
+    fulldataframes=[pd.concat([dataframesprevious[i],dataframes[i]]).reset_index(drop=True) for i in range(len(dataframes))]  	
+    price=prices[number]	
+    fulldataframe=fulldataframes[number]	
+    real=fulldataframe['tasa']         
+    precios=fulldataframe['precios']
+    #build predicted prices
+    previous_real_prices_first_half=price['precios'].tolist()[0:7]
+    predicted_rates_first_half=prediccion.tolist()[0:7]	
+    previous_real_prices_second_half=price['precios'].tolist()[7:]
+    predicted_rates_second_half=prediccion.tolist()[7:]		
+    precios_prediccion=predicted_prices(predicted_rates_first_half,previous_real_prices_first_half)+predicted_prices(predicted_rates_second_half,previous_real_prices_second_half)	
+    #
+    x=dataframes[number]
+    x['pronóstico tasa']=prediccion.tolist()[7:]	
+    x['pronóstico precios']=precios_prediccion[7:]
+    x=x[['fecha','precios','pronóstico precios','tasa','pronóstico tasa']]		
+    x=x.reset_index(drop=True)		
+    error_precios=RSME(precios[0:7],precios_prediccion[0:7])	
+    return render_template('pronosticoNAFTRAC.html', values_prediccion=prediccion,values_real=real,values_precios=precios, values_precios_prediccion=precios_prediccion,labels=fecha,fechaInicio=fechaInicio,fechaFin=fechaFin,tables=[x.to_html(classes='table')],error_precios=error_precios)
  
    	
    
