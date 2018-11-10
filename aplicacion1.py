c198168880f150afe635e4813ce4d88c191956e8
pcmProject092018 <acampos@emsmxgroup.com>
2018-11-10 13:22:52 -0600

    update

--- aplicacion1.py
+++ aplicacion1.py
@@ -228,7 +228,38 @@ def pronosticoALSEA():
     x=x.reset_index(drop=True)		
     error_precios=RSME(precios[0:7],precios_prediccion[0:7])	
     return render_template('pronosticoALSEA.html', values_prediccion=prediccion,values_real=real,values_precios=precios, values_precios_prediccion=precios_prediccion,labels=fecha,fechaInicio=fechaInicio,fechaFin=fechaFin,tables=[x.to_html(classes='table')],error_precios=error_precios)
- 	
+ 
+@app.route("/pronosticoGFNORTEO",methods=['POST','GET']) 
+def pronosticoGFNORTEO():   
+    number = request.form.get('ejemplo',type=int)
+    dfs=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/predicciones/banorte/*.csv'))]	
+    prediccion=dfs[number]['prediccion']    
+    fecha=dfs[number]['fecha'] 	      	       
+    fechaInicio=fecha[0]  
+    fechaFin=fecha[6]
+    dataframes=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/dataframes/banorte/*.csv'))]
+    dataframesprevious=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/dataframesprevious/banorte/*.csv'))]	
+    prices=[pd.read_csv(file,header=0) for file in sorted(glob.glob('model/precios/banorte/*.csv'))]  
+    fulldataframes=[pd.concat([dataframesprevious[i],dataframes[i]]).reset_index(drop=True) for i in range(len(dataframes))]  	
+    price=prices[number]	
+    fulldataframe=fulldataframes[number]	
+    real=fulldataframe['tasa']         
+    precios=fulldataframe['precios']
+    #build predicted prices
+    previous_real_prices_first_half=price['precios'].tolist()[0:7]
+    predicted_rates_first_half=prediccion.tolist()[0:7]	
+    previous_real_prices_second_half=price['precios'].tolist()[7:]
+    predicted_rates_second_half=prediccion.tolist()[7:]		
+    precios_prediccion=predicted_prices(predicted_rates_first_half,previous_real_prices_first_half)+predicted_prices(predicted_rates_second_half,previous_real_prices_second_half)	
+    #
+    x=dataframes[number]
+    x['pronóstico tasa']=prediccion.tolist()[7:]	
+    x['pronóstico precios']=precios_prediccion[7:]
+    x=x[['fecha','precios','pronóstico precios','tasa','pronóstico tasa']]		
+    x=x.reset_index(drop=True)		
+    error_precios=RSME(precios[0:7],precios_prediccion[0:7])	
+    return render_template('pronosticoGFNORTEO.html', values_prediccion=prediccion,values_real=real,values_precios=precios, values_precios_prediccion=precios_prediccion,labels=fecha,fechaInicio=fechaInicio,fechaFin=fechaFin,tables=[x.to_html(classes='table')],error_precios=error_precios)
+  
    
 if __name__ == "__home__":
     #app.run(debug=True,port=5000)
